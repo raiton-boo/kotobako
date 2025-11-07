d20d205451a9ab7b3d6a7022816e5f659dfdda9d
らいとん <raitosongwei@gmail.com>
2025-11-08 00:00:55 +0900

    基本的なWebサイトの構築

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://raiton-boo.github.io',
+  site: 'https://yourusername.github.io',
   base: '/kotobako',
-
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
